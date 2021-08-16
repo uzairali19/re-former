1e55a83f8bb771bec4ada46642b99d02b6c7930f
Uzair <uzairali19b@gmail.com>
2021-08-16 16:13:51 +0500

    update form logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to root_path
+  end
+
   private
 
   def user_params
